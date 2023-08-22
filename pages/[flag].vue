e9ce31fcf2f5c4e5709c29526cf25cb87248ee43
s.khaki <sara.khaki117@gmail.com>
2023-08-22 12:45:58 +0330

    first commit

---
+++ pages/[flag].vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <h1>Flag Details</h1>
+        <br>
+        <h2>{{ flagInfo.flag }}</h2>
+
+        {{ flagInfo.code }}
+
+
+        <div style="background-color: aquamarine;">
+
+            <ul>
+                <li v-for="(item, i) in resultBorder" :key="i">
+                    <h1>
+                        All Border Of <span style="color:blue ;border-bottom: 2px solid blue;">{{ item.name.common }}</span> :
+                    </h1>
+                    <img class="flag" :src="item.flags.img" :alt="item.flags.alt">
+
+                    <div>
+                        <span v-for="border in item.borders">
+                            {{ border }} 
+                            <br>
+                        </span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <!-- <h5>{{ flagInfo.ccn3 }}</h5> -->
+
+        <!-- <h4 v-for="b in flagInfo.borders">
+            {{ b }}
+        </h4> -->
+    </div>
+</template>
+
+<script setup>
+
+const flagInfo = useRoute().params;
+console.log(flagInfo)
+
+const code = ref();
+
+const url = `https://restcountries.com/v3.1/name/${flagInfo.flag}`
+
+const resultBorder = await $fetch(url).catch((error) => error.data)
+
+console.log(url)
+
+</script>
+
+<style>
+img.flag {
+    width: 280px;
+    height: 150px;
+    background-color: #ddd;
+    display: block;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <!-- query Link(optional) :  <NuxtLink to="/city?id=55555">Link parameter</NuxtLink>
+       <br>
+       parameter :  <NuxtLink to="/city/55555">Link parameter</NuxtLink>
+        <br>
+        <NuxtLink :to="{
+            name: 'city-id',
+            params: {
+                id: 1234
+            }
+        }"> Link Go To Input</NuxtLink> -->
+        <br>
+        <div class="flag_sec">
+            <ul>
+                <li v-for="(item, i ) in   result  " :key="i">
+                    <!-- <NuxtLink to="/flag"> -->
+                    <NuxtLink style="border: 2px solid #000;" :to="{
+                        name: 'flag',
+                        params: {
+                            flag: item.name.common,
+                            // img: item.flags.png,
+                            // ccn3: item.ccn3 // از این همم خواستم استفاده کنم، نوشت این میس شده (miss)
+                            // borders: item.borders  /// ?? آاریاه خوانده نشد در لینک
+                        },
+                    }">
+                        <img class="flag" :src="item.flags.png" :alt="item.flags.alt">
+                        <span style="color: black;font-weight: bold;">{{ item.name.common }}</span>
+                    </NuxtLink>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+const result = await $fetch('https://restcountries.com/v3.1/all').catch((error) => error.data)
+
+console.log(result)
+</script>
+
+<style>
+.flag_sec {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+
+.flag_sec ul {
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.flag_sec ul li {
+    position: relative;
+    width: 280px;
+    height: 150px;
+    overflow: hidden;
+    border: 1px solid #000;
+    margin: 10px;
+}
+
+.flag_sec ul li a {
+    position: relative;
+    width: 100%;
+    display: block;
+    height: 100%;
+}
+
+.flag_sec ul li a img {
+    object-fit: cover;
+    height: 85%;
+    width: 100%;
+}
+</style>
